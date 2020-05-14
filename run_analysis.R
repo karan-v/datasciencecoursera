c33d835acf0a6663ae99007936c6bda47adb9448
Karan Vaish <kvaish1264@gmail.com>
2020-05-14 08:03:30 +0530

    added course 3 project

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+library(data.table)
+library(reshape2)
+path = getwd()
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl,destfile = "./dataset.zip",method = "curl")
+unzip(zipfile = "./dataset.zip")
+
+labels <- fread(file.path(path,"UCI HAR Dataset/activity_labels.txt")
+                ,col.names = c("labels","class"))
+features <- fread(file.path(path,"UCI HAR Dataset/features.txt")
+                  ,col.names = c("index","fname"))
+
+featreq <- grep("(mean|std)\\(\\)",features$fname)
+
+#reading the training data
+train <- fread(file.path(path,"UCI HAR Dataset/train/X_train.txt"))[,featreq,with = FALSE]
+labels_train <- fread(file.path(path,"UCI HAR Dataset/train/y_train.txt"),
+                      col.names = c("class"))
+sub_train <- fread(file.path(path,"UCI HAR Dataset/train/subject_train.txt"),
+                   col.names = c("subname"))
+train <- cbind(train,labels_train,sub_train)
+
+#reading the test data
+test <- fread(file.path(path,"UCI HAR Dataset/test/X_test.txt"))[,featreq,with = FALSE]
+labels_test <- fread(file.path(path,"UCI HAR Dataset/test/y_test.txt"),
+                     col.names = c("class"))
+sub_test <- fread(file.path(path,"UCI HAR Dataset/test/subject_test.txt"),
+                  col.names = c("subname"))
+test <- cbind(test,labels_test,sub_test)
+
+#merging test and train data together
+merged <- rbind(train,test)
+
+merged[["class"]] <- factor(merged[, class], levels = labels[["labels"]], labels = labels[["class"]])
+merged[["subname"]] <- as.factor(merged[ ,subname])
+
+merged <- reshape2::melt(merged,id = c("class","subname"))
+merged <- reshape2::dcast(merged,class+subname~variable,mean)
+
+data.table::fwrite(x = merged,file = "tidyData.txt",quote = FALSE)
